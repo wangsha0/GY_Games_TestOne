76d774b89dbf727325b6b969098394de9af3dde5
wangsha0 <807435316@qq.com>
2017-12-31 14:06:56 +0800

    add Wrong: git-receive-pack

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test");   
 		
+//		git-receive-pack : 没有验证邮箱？
 		System.out.println("有时配置正确但出现can not open URL 是网络不好");
 		
 
